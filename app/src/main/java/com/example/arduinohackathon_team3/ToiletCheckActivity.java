3bfa94c1d38eda8e0c339f18ba4617f87364830d
HeoYunJeong <yund_272@naver.com>
2019-08-19 13:07:50 +0900

    xml 틀 완성
    +자바 연결, 세부 디자인 적용 필요

--- app/src/main/java/com/example/arduinohackathon_team3/ToiletCheckActivity.java
+++ app/src/main/java/com/example/arduinohackathon_team3/ToiletCheckActivity.java
@@ -3,6 +3,8 @@ package com.example.arduinohackathon_team3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ToiletCheckActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class ToiletCheckActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toilet_check);
+
+        Spinner dropdown = (Spinner) findViewById(R.id.sprState);
+        String[] items = new String[]{"50주년 기념관", "인문사회관", "제1과학관", "제2과학관", "조형예술관", "중앙도서관", "학생누리관"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ToiletCheckActivity.this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        Spinner dropdown2 = (Spinner) findViewById(R.id.sprState2);
+        String[] items2 = new String[]{"1층", "2층 왼쪽", "2층 오른쪽"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(ToiletCheckActivity.this, android.R.layout.simple_spinner_dropdown_item, items2);
+        dropdown.setAdapter(adapter2);
     }
 }
