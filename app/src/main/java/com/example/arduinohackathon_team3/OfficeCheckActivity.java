392486c195df320bdc2627877c250dbe4dcb2018
HeoYunJeong <yund_272@naver.com>
2019-08-19 10:50:29 +0900

    페이지 중간 백업

---
+++ app/src/main/java/com/example/arduinohackathon_team3/OfficeCheckActivity.java
@@ -0,0 +1,14 @@
+package com.example.arduinohackathon_team3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OfficeCheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_office_check);
+    }
+}
---
+++ app/src/main/java/com/example/arduinohackathon_team3/ProfMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.arduinohackathon_team3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prof_main);
+    }
+}
