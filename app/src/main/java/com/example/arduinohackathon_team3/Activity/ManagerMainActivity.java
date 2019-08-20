346849dd026ea173fbe82c942705f715d9ea3b57
KimMinji <happy0652@naver.com>
2019-08-20 19:48:05 +0900

    //관리자, 학생 메인화면 로그아웃

--- app/src/main/java/com/example/arduinohackathon_team3/Activity/ManagerMainActivity.java
+++ app/src/main/java/com/example/arduinohackathon_team3/Activity/ManagerMainActivity.java
@@ -1,13 +1,19 @@
 package com.example.arduinohackathon_team3.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.arduinohackathon_team3.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ManagerMainActivity extends AppCompatActivity {
 
@@ -34,5 +40,40 @@ public class ManagerMainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_logout:
+                logout();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //로그아웃을 처리한다.
+    private void logout(){
+        FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+        try{
+            if(MainActivity.mGoogleSignInClient != null) {
+                MainActivity.mGoogleSignInClient.signOut();
+            }
+            mFirebaseAuth.signOut();
+            Toast.makeText(this,"로그아웃 되었습니다.",Toast.LENGTH_LONG).show();
+            Intent i=new Intent(this, com.example.arduinohackathon_team3.Activity.MainActivity.class);
+            startActivity(i);
+            //finish();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/arduinohackathon_team3/Activity/StudentMainActivity.java
+++ app/src/main/java/com/example/arduinohackathon_team3/Activity/StudentMainActivity.java
@@ -1,13 +1,18 @@
 package com.example.arduinohackathon_team3.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.arduinohackathon_team3.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class StudentMainActivity extends AppCompatActivity {
 
@@ -44,5 +49,40 @@ public class StudentMainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_logout:
+                logout();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //로그아웃을 처리한다.
+    private void logout(){
+        FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+        try{
+            if(MainActivity.mGoogleSignInClient != null) {
+                MainActivity.mGoogleSignInClient.signOut();
+            }
+            mFirebaseAuth.signOut();
+            Toast.makeText(this,"로그아웃 되었습니다.",Toast.LENGTH_LONG).show();
+            Intent i=new Intent(this, com.example.arduinohackathon_team3.Activity.MainActivity.class);
+            startActivity(i);
+            //finish();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
