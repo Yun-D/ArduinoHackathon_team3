fb503b0c74a6ea1474aeaf1f42aa64adaa28ef97
KimMinji <happy0652@naver.com>
2019-08-20 18:39:46 +0900

    //앱이름변경, 버튼이벤트

--- app/src/main/java/com/example/arduinohackathon_team3/Activity/ManagerMainActivity.java
+++ app/src/main/java/com/example/arduinohackathon_team3/Activity/ManagerMainActivity.java
@@ -2,7 +2,10 @@ package com.example.arduinohackathon_team3.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.arduinohackathon_team3.R;
 
@@ -12,5 +15,24 @@ public class ManagerMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_main);
+
+        Button mBtnToiletCheck=findViewById(R.id.btnToiletCheck);
+        Button mBtnClassroomCheck=findViewById(R.id.btnClassroomCheck);
+
+        mBtnToiletCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),ToiletCheckActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mBtnClassroomCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),ClassroomCheckActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/arduinohackathon_team3/Activity/StudentMainActivity.java
+++ app/src/main/java/com/example/arduinohackathon_team3/Activity/StudentMainActivity.java
@@ -2,7 +2,10 @@ package com.example.arduinohackathon_team3.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.arduinohackathon_team3.R;
 
@@ -12,5 +15,34 @@ public class StudentMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
+
+        Button mBtnToiletCheck=findViewById(R.id.btnToiletCheck);
+        Button mBtnClassroomCheck=findViewById(R.id.btnClassroomCheck);
+        Button mBtnOfficeCheck=findViewById(R.id.btnOfficeCheck);
+
+
+        mBtnToiletCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),ToiletCheckActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mBtnClassroomCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),ClassroomCheckActivity.class);
+                startActivity(i);
+            }
+        });
+
+        mBtnOfficeCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),OfficeCheckActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
